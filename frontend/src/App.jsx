973bf7a50b8804e48d0b1ad3c278ba894dbd570e
Darshan-Domadiya <darshandomadiya3525@gmail.com>
2024-07-25 21:50:35 +0530

    created pages and set up routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
 
 const App = () => {
-  return <div className="bg-danger">App</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 };
 
 export default App;
